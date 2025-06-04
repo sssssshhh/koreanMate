1986852f18dd987fe77175b244356bec0f9751b7
sssssshhh <eyesh0011@gmail.com>
2025-06-04 23:55:49 +0900

    add shadcn

--- app/features/auth/pages/login.tsx
+++ app/features/auth/pages/login.tsx
@@ -1,7 +1,9 @@
+import { Button } from "~/components/ui/button";
+
 export default function Login() {
     return (
-      <div className="container mx-auto px-4 py-8 text-black">
-        Login
+      <div className="container mx-auto px-4 py-8">
+        <Button>test</Button>
       </div>
     );
   }
